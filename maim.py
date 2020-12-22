e30d17e576fb4944ddcc7f7bbd51d32964ab762e
p.sidorov <p.sidorov@dev.userstory.ru>
2020-12-22 17:18:13 +0700

    format output

--- maim.py
+++ maim.py
@@ -30,7 +30,8 @@ def multiply(x,y, num=1000):
             final_y.append(temp_y)
     return np.array(final_x),np.array(final_y)
 
-
+def predict(model,number):
+    return model.predict(np.asarray([number]))
 
 def print_matrix(temp):
     print(np.array(temp).reshape(7,5))
@@ -58,17 +59,17 @@ def train():
     model.fit(X_train, y_train, epochs=10, batch_size=128, validation_data = (X_test, y_test))
     for dat in x_data:
         print_matrix(dat)
-        predicted_outputs = model.predict(np.asarray([dat]))
-        print(np.argmax(predicted_outputs, axis=1))
+        predicted_outputs = predict(model,dat)
+        print('predicted number is',np.argmax(predicted_outputs, axis=1))
     model.save('model.h5')
 
 def test():
     model = keras.models.load_model('model.h5')
     data = load_file('testfile')
     number =  [int(i) for i in data.split()]
-    predicted_outputs = model.predict(np.asarray([number]))
+    predicted_outputs = predict(model,number)
     print_matrix(number)
-    print(np.argmax(predicted_outputs, axis=1))
+    print('predicted number is',np.argmax(predicted_outputs, axis=1))
     
 
 funcs = {
